3b4b6c52d12630dc5a8f9cc04e045051d32ee8eb
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-11-05 17:45:17 +0200

    Comment out module export definition

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+// Object.defineProperty(exports, "__esModule", { value: true });
 let todoCounter = 0;
 function getById(id) {
     const el = document.getElementById(id);
